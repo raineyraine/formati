aefb84d98d1de2241e2c04a7ddb33289be9824a6
Raine <randomrandom.epic.epic@gmail.com>
2025-11-27 22:13:47 -0500

    feat: add package.json, eslint and prettier

---
+++ eslint.config.ts
@@ -0,0 +1,43 @@
+import eslint from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import roblox from "eslint-plugin-roblox-ts";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default defineConfig(
+	eslint.configs.recommended,
+	tseslint.configs.recommended,
+	roblox.configs.tsRecommendedCompat,
+	roblox.configs.recommended,
+	reactHooks.configs.flat.recommended,
+
+	{
+		plugins: {
+			"simple-import-sort": simpleImportSort,
+		},
+		rules: {
+			"simple-import-sort/imports": "error",
+			"simple-import-sort/exports": "error",
+		},
+	},
+	{
+		plugins: {
+			"unused-imports": unusedImports,
+		},
+		rules: {
+			"unused-imports/no-unused-imports": "error",
+			"unused-imports/no-unused-vars": [
+				"warn",
+				{
+					varsIgnorePattern: "^_",
+					argsIgnorePattern: "^_",
+				},
+			],
+		},
+	},
+	globalIgnores(["out/**"]),
+	prettierRecommended
+);
